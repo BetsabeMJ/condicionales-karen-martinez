9e5da5122995b3ad8bf093ae1fe77fc3ca8a02c2
Betsabe Martinez <betsabe.martinez.jasso@gmail.com>
2025-01-18 00:34:01 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// console.log("Hola");
+
+// **************************************************************
+// SECCIÓN IMAGEN CON BORDE
+
+const imagen = document.querySelector("img");
+
+imagen.addEventListener("click", () => {
+  // img.style.border.red;
+  imagen.style.border = "red";
+});
+
+// **************************************************************
+// SECCIÓN STICKERS
+
+// **************************************************************
+// SECCIÓN PASSWORD
