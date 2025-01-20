4c7181882be57787980449626c1fc6da76a9cc5e
Betsabe Martinez <betsabe.martinez.jasso@gmail.com>
2025-01-19 21:35:19 -0600

    se agregan últimas ediciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 // console.log("Hola");
-
 // **************************************************************
 // SECCIÓN IMAGEN CON BORDE
 const imagenPlaneta = document.querySelector(".imagen-planeta");
 
 imagenPlaneta.addEventListener("click", () => {
-  imagenPlaneta.style.border = "5px groove #00fff6";
+  const imagenPlanetaBorder = (imagenPlaneta.style.border = "2px groove red");
+  if (imagenPlaneta === imagenPlaneta) {
+    imagenPlanetaBorder;
+  } else {
+    imagenPlaneta;
+  }
+  //NO SUPE CÓMO HACER PARA QUITAR EL BORDE.
 });
-
 // **************************************************************
 // SECCIÓN STICKERS
 const stickers = document.querySelector(".stickers");
@@ -16,15 +20,16 @@ const sticker2 = document.querySelector(".sticker2");
 const sticker3 = document.querySelector(".sticker3");
 const stickersTotal = document.querySelector(".stickers-total");
 const stickerButton = document.querySelector(".sticker-button");
+
 stickerButton.addEventListener("click", () => {
-  if (sticker1 + sticker2 + sticker3 > 10) {
-    stickersTotal.innerHTML +=
-      "Llevas " + sticker1 + sticker2 + sticker3 + " stickers";
+  const total =
+    Number(sticker1.value) + Number(sticker2.value) + Number(sticker3.value);
+  if (total <= 10) {
+    stickersTotal.innerHTML = "Llevas " + total + " stickers";
   } else {
     stickersTotal.innerHTML = "Llevas demaciados stickers";
   }
 });
-
 // **************************************************************
 // SECCIÓN PASSWORD
 const primero = document.querySelector("#primero");
@@ -34,13 +39,12 @@ const passwordButton = document.querySelector(".password-button");
 const validarPassword = document.querySelector(".validar-password");
 
 passwordButton.addEventListener("click", () => {
-  if (primero === 9) {
-  }
-  if (segundo === 1) {
-  }
-  if (tercero === 1) {
-    validarPassword.innerHTML = "Password correcto";
+  const tresSelect = primero.value + segundo.value + tercero.value;
+  if (tresSelect === "911") {
+    validarPassword.innerHTML = "Password 1 correcto";
+  } else if (tresSelect === "914") {
+    validarPassword.innerHTML = "Password 2 correcto";
   } else {
-    validarPassword.innerHtml = "Password incorrecto. Vuelve a intentarlo";
+    validarPassword.innerHTML = "Password incorrecto. Vuelve a intentarlo";
   }
 });
